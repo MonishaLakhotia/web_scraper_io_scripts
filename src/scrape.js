ee781d9da3cf4a8808d1b43852e00166bab7abe6
Manyu Lakhotia <lakemonyew@gmail.com>
2021-11-13 15:25:07 -0600

    Adds csv support

--- src/scrape.js
+++ src/scrape.js
@@ -1,12 +1,35 @@
 const webscraper = require('web-scraper-headless');
 const fs = require('fs-extra');
+const json2csv = require('json2csv');
+const jtu = require('json-test-utility');
+const jr = jtu.jsonRefactor;
+
+function trim(thing) {
+  return (typeof thing === 'string')
+    ? thing.trim().replace(/\s{2,}/g, ' ').replace(/,/g, ';')
+    : thing;
+}
 
 async function main() {
   const sitemap = fs.readJSONSync(__dirname + '/sitemap.json');
 
-  const options = {delay: 10, pageLoadDelay: 10, browser: 'headless'} // optional delay, pageLoadDelay and browser
-  const scraped = await webscraper(sitemap, options);
-  console.log(JSON.stringify(scraped, null, 2));
+  const scrapOptions = {delay: 10, pageLoadDelay: 10, browser: 'headless'} // optional delay, pageLoadDelay and browser
+  const scraped = await webscraper(sitemap, scrapOptions);
+  // ensure that scraped data is an array
+  let cleanedData = !Array.isArray(scraped) ? [scraped] : scraped;
+  // trim string values
+  cleanedData = cleanedData.map(s => jr.fromKeyValArray(jr.toKeyValArray(s).map(kv => ({key:kv.key,value:trim(kv.value)}))));
+
+  // print scraped data in json format
+  // console.log(JSON.stringify(cleanedData, null, 2));
+
+  // quote: '' removes quotes from fields/headers
+  // const parserOptions = { quote: '' }
+  // OR for no customizations
+  const parserOptions = {};
+  const csv = json2csv.parse(cleanedData, parserOptions);
+  // print scraped data in csv format
+  console.log(csv);
 }
 
 main();
